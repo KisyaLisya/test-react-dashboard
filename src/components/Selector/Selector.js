726f4e88032d9ee0e6e6e54ccc883ad63eeb69c3
Jenny <mark.cardinal@yandex.ru>
2019-03-01 15:23:27 +0400

    add dropdown

---
+++ src/components/Selector/Selector.js
@@ -0,0 +1,102 @@
+import React, { Component } from 'react';
+
+import { makeClass, makePlaceholder } from '../../active/Utils';
+import './Selector.less';
+
+export default class Selector extends Component {
+
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			opened: false,
+			search: '',
+		}
+
+		this.onSearch = this.onSearch.bind(this);
+		this.clearSearch = this.clearSearch.bind(this);
+	}
+
+	applyFilter(filter, value) {
+		if (filter) {
+			const regex = new RegExp(`^${filter}`);
+			return value.search(regex) !== -1;
+		} else {
+			return true;
+		}
+	}
+
+	onSearch(event) {
+		event.stopPropagation();
+		const value = event.target.value;
+
+		this.setState({
+			search: value
+		})
+	}
+
+	clearSearch() {
+		this.setState({
+			search: ''
+		})
+	}
+
+	render() {
+		const { className = '', opened = false, elements = [], value = '', onSelect, onToggle } = this.props;
+		const { search } = this.state;
+
+		const list = elements
+			.filter(({ title }) => this.applyFilter(search, title))
+			.map((el) => {
+				const { id, title } = el;
+				return(
+					<li
+						key={id}
+						className="dropdown-item"
+						onClick={() => {
+							this.clearSearch();
+							onSelect(id);
+						}}
+					>
+						{title}
+					</li>
+				)
+			});
+
+		let body = null;
+
+		if (opened) {
+			body = (
+				<React.Fragment>
+					<input
+						className="dropdown-toggle form-control"
+						value={search}
+						onChange={this.onSearch}
+						autoFocus
+					/>
+					<ul className={`dropdown-menu show`}>
+						{list}
+					</ul>
+				</React.Fragment>
+			);
+		} else {
+			body = (
+				<div
+					className="dropdown-toggle"
+					onClick={onToggle}
+				>
+					{value}
+				</div>
+			);
+		}
+
+		return(
+			<div
+				className={`btn btn-primary w-25 dropdown ${opened ? 'show' : ''}`}
+				onClick={(e) => e.stopPropagation()}
+			>
+				{body}
+			</div>
+		);
+	}
+}
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,28 +1,81 @@
 import React, { Component } from 'react';
 
-import { makeClass } from '../../active/Utils';
 import './App.less';
 
 import Button from '../../components/Button';
 import Input from '../../components/Input';
 import Checkbox from '../../components/Checkbox';
 import CheckboxSelector from '../../components/CheckboxSelector';
-import Table from '../../components/Table';
+import Selector from '../../components/Selector';
 
+import Table from '../../components/Table';
 import Header from '../Header';
 
 export default class App extends Component {
 
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			opened: false,
+			selected: 'Dropdown',
+			elements: [
+				{ id: 0, title: 'elem0' },
+				{ id: 1, title: '22elem1' },
+				{ id: 2, title: 'elem2' },
+				{ id: 3, title: '6elem3' },
+				{ id: 4, title: '66elem4' },
+				{ id: 5, title: '1elem5' },
+			]
+		};
+		this.onCloseDropdowns = this.onCloseDropdowns.bind(this);
+		this.onToggle = this.onToggle.bind(this);
+		this.onSelect = this.onSelect.bind(this);
+	}
+
+	onCloseDropdowns(event) {
+		this.setState({
+			opened: false
+		})
+	}
+
+	onToggle(event) {
+		event.stopPropagation();
+		const { opened } = this.state;
+
+		this.setState({
+			opened: !opened
+		})
+	}
+
+	onSelect(id) {
+		const selElem = this.state.elements.find((el) => el.id === id);
+
+		if (!selElem) return;
+
+		this.setState({
+			opened: false,
+			selected: selElem.title
+		})
+	}
+
 	render() {
-		const props = this.props;
-		const propsClass = makeClass(props.className);
+		const { className = '' } = this.props;
+		const { opened, elements, selected } = this.state;
 
 		return(
-			<div>
+			<div onClick={this.onCloseDropdowns}>
 				<Header
 					name="My App"
 				/>
 				<main className="jumbotron">
+					<Selector
+						opened={opened}
+						value={selected}
+						elements={elements}
+						onToggle={this.onToggle}
+						onSelect={this.onSelect}
+					/>
 					<Table />
 				</main>
 			</div>
